ac8970e62f3fbce3bed7ec072b8335bf957454bf
DenisSoloviev1 <solovievdenis2004@gmail.com>
2024-06-23 23:31:33 +0300

    1

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "../src/app/App.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app/App.jsx';
+
 
 const root = document.getElementById("root"); 
 ReactDOM.createRoot(root).render(<App />);
